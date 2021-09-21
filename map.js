a25ac8bfce4199012b330fc5c1baae3670ac3f03
Byron Cole Lawlis <bclaw8300@gmail.com>
2021-09-21 07:57:42 -0600

    added to map

--- map.js
+++ map.js
@@ -1,16 +1,36 @@
-var map = L.map('map').setView([0, 0], 1);
-
-// L.imageOverlay('2021-CFD-MAP.png').addTo(map);
+var map = new L.map('map').setView([0, 0], 1);
+var mapSW = [0, 2048], // set bounds in variables
+    mapNE = [2048, 0];
 
 L.tileLayer('CFD-tiles/{z}/{x}/{y}.png', {
-    minZoom: 1,
+    center: [0, 0],
+    minZoom: 2,
     maxZoom: 3,
-    continuousWorld: false,
     noWrap: true,
+    cursor: true,
+    crs: L.CRS.Simple, // converts to points
 }).addTo(map);
 
-var marker = L.marker([1, 100]).addTo(map);
-marker.bindPopup('popupContent').openPopup();
+map.setMaxBounds(new L.LatLngBounds(
+    map.unproject(mapSW, map.getMaxZoom()),
+    map.unproject(mapNE, map.getMaxZoom())
+));
 
-var marker2 = L.marker([-50,-40]).addTo(map);
-marker2.bindPopup('popupContent').openPopup();
+// Markers and Popups
+var marker = L.marker([0, 0], {
+    draggable: true,
+}).addTo(map);
+marker.on('click', function(ev) {
+    alert(ev.latlng); // ev is an event object (MouseEvent in this case)
+});  
+
+var marker1 = L.marker([1, 100]).addTo(map);
+marker1.bindPopup('<h6 class="text-success">My Favorite Spot</h6>').openPopup();
+
+var marker2 = L.marker(map.unproject([900, 1500],map.getMaxZoom())).addTo(map);
+marker2.bindPopup('<h6 class="text-info">Horses & Shit</h6>').openPopup();
+
+var marker3 = L.marker(map.unproject([1650, 1650], map.getMaxZoom())).addTo(map).bindPopup('<h6>Our Heroes</h6>').openPopup();
+
+
+
